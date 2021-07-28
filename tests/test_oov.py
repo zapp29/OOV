8a46e37e237e557f57dc88cf8035770185a5a382
Piotr Tchir <ptchir@interia.pl>
2021-07-28 20:06:42 +0200

    change module name

--- tests/test_oov.py
+++ tests/test_oov.py
@@ -1,6 +1,7 @@
-from oov import OOV
 from types import ModuleType
+
 import pytest
+from src.OOV.OOV import OOV as OOV
 
 
 class TestCreateObject:
@@ -15,18 +16,18 @@ class TestCreateObject:
         """
 
         def test_object_length(self):
-            result_1 = len(OOV(["pandas", "numpy"]).parsed_objs)
+            result_1 = len(OOV(["collections.abc", "typing"]).parsed_objs)
             assert result_1 == 2
 
         def test_object_keys(self):
-            result_1 = list(OOV(["pandas", "numpy"]).parsed_objs)[0]
-            result_2 = list(OOV(["pandas", "numpy"]).parsed_objs)[1]
-            assert result_2 == "numpy"
-            assert result_1 == "pandas"
+            result_1 = list(OOV(["collections.abc", "typing"]).parsed_objs)[0]
+            result_2 = list(OOV(["collections.abc", "typing"]).parsed_objs)[1]
+            assert result_2 == "typing"
+            assert result_1 == "collections.abc"
 
         def test_object_value_types(self):
-            result_1 = OOV(["pandas", "numpy"]).parsed_objs['pandas']
-            result_2 = OOV(["pandas", "numpy"]).parsed_objs['numpy']
+            result_1 = OOV(["collections.abc", "typing"]).parsed_objs["collections.abc"]
+            result_2 = OOV(["collections.abc", "typing"]).parsed_objs["typing"]
             assert isinstance(result_1, ModuleType)
             assert isinstance(result_2, ModuleType)
 
@@ -39,15 +40,15 @@ class TestCreateObject:
         """
 
         def test_object_length(self):
-            result_1 = len(OOV("pandas").parsed_objs)
+            result_1 = len(OOV("collections.abc").parsed_objs)
             assert result_1 == 1
 
         def test_object_keys(self):
-            result_2 = list(OOV("pandas").parsed_objs)[0]
-            assert result_2 == "pandas"
+            result_2 = list(OOV("collections.abc").parsed_objs)[0]
+            assert result_2 == "collections.abc"
 
         def test_object_value_types(self):
-            result_3 = OOV("pandas").parsed_objs['pandas']
+            result_3 = OOV("collections.abc").parsed_objs["collections.abc"]
             assert isinstance(result_3, ModuleType)
 
     class TestTwoPackagesImpFalse:
@@ -59,27 +60,29 @@ class TestCreateObject:
         """
 
         def test_object_length(self):
-            import pandas
-            import numpy
-            result_1 = len(OOV([pandas, numpy]).parsed_objs)
+            import collections.abc
+            import typing
+
+            result_1 = len(OOV([collections.abc, typing]).parsed_objs)
             assert result_1 == 2
 
         def test_object_keys(self):
-            import pandas
-            import numpy
-            result_1 = list(OOV([pandas, numpy]).parsed_objs)[0]
-            result_2 = list(OOV([pandas, numpy]).parsed_objs)[1]
-            assert result_2 == "numpy"
-            assert result_1 == "pandas"
+            import collections.abc
+            import typing
+
+            result_1 = list(OOV([collections.abc, typing]).parsed_objs)[0]
+            result_2 = list(OOV([collections.abc, typing]).parsed_objs)[1]
+            assert result_2 == "typing"
+            assert result_1 == "collections.abc"
 
         def test_object_value_types(self):
-            import pandas
-            import numpy
-            result_1 = OOV([pandas, numpy]).parsed_objs['pandas']
-            result_2 = OOV([pandas, numpy]).parsed_objs['numpy']
+            import collections.abc
+            import typing
+
+            result_1 = OOV([collections.abc, typing]).parsed_objs["collections.abc"]
+            result_2 = OOV([collections.abc, typing]).parsed_objs["typing"]
             assert isinstance(result_1, ModuleType)
             assert isinstance(result_2, ModuleType)
-
 
     class TestOnePackageImpFalse:
         """TEST CASE 4.
@@ -92,23 +95,26 @@ class TestCreateObject:
         """
 
         def test_object_length(self):
-            import pandas
-            result_1 = len(OOV([pandas]).parsed_objs)
-            result_2 = len(OOV(pandas).parsed_objs)
+            import collections.abc
+
+            result_1 = len(OOV([collections.abc]).parsed_objs)
+            result_2 = len(OOV(collections.abc).parsed_objs)
             assert result_1 == 1
             assert result_2 == 1
 
         def test_object_keys(self):
-            import pandas
-            result_1 = list(OOV([pandas]).parsed_objs)[0]
-            result_2 = list(OOV(pandas).parsed_objs)[0]
-            assert result_1 == "pandas"
-            assert result_2 == "pandas"
+            import collections.abc
+
+            result_1 = list(OOV([collections.abc]).parsed_objs)[0]
+            result_2 = list(OOV(collections.abc).parsed_objs)[0]
+            assert result_1 == "collections.abc"
+            assert result_2 == "collections.abc"
 
         def test_object_value_types(self):
-            import pandas
-            result_1 = OOV([pandas]).parsed_objs['pandas']
-            result_2 = OOV(pandas).parsed_objs['pandas']
+            import collections.abc
+
+            result_1 = OOV([collections.abc]).parsed_objs["collections.abc"]
+            result_2 = OOV(collections.abc).parsed_objs["collections.abc"]
             assert isinstance(result_1, ModuleType)
             assert isinstance(result_2, ModuleType)
 
@@ -130,12 +136,13 @@ class TestCreateObject:
             with pytest.raises(ModuleNotFoundError):
                 OOV("pythonsucks")
 
+
 class TestUpdateDictInplace:
     """Test _update_dict_inplace method."""
 
     def test__update_dict_inplace(self):
         d = {}
-        obj = OOV("pandas")
+        obj = OOV("collections.abc")
         obj._update_dict_inplace(d, "k1", "k2", 123)
         assert d["k1"]["k2"] == 123
         obj._update_dict_inplace(d, "k1", "k2", 321)
@@ -145,35 +152,37 @@ class TestUpdateDictInplace:
         obj._update_dict_inplace(d, "k4", "k2", 4321)
         assert d["k4"]["k2"] == 4321
 
+
 class TestViewIssubclass:
     """Test view_issubclass method."""
 
     def test_view_issubclass(self):
-        import input.dummy_module as dummy_module
+        import tests.input.dummy_module as dummy_module
+
         obj = OOV(dummy_module)
         result = obj.view_issubclass()
-        
-        assert result['MainClass1']['MainClass1'] == 1
-        assert result['MainClass2']['MainClass2'] == 1
-        assert result['MainClass3']['MainClass3'] == 1
 
-        assert result['ChildClass1']['ChildClass1'] == 1
-        assert result['ChildClass2']['ChildClass2'] == 1
+        assert result["MainClass1"]["MainClass1"] == 1
+        assert result["MainClass2"]["MainClass2"] == 1
+        assert result["MainClass3"]["MainClass3"] == 1
 
-        assert result['ChildClass1']['MainClass1'] == 1
-        assert result['ChildClass1']['MainClass2'] == 1
-        assert result['ChildClass1']['MainClass3'] == 1
-        assert result['ChildClass1']['MainClass4'] == 0
+        assert result["ChildClass1"]["ChildClass1"] == 1
+        assert result["ChildClass2"]["ChildClass2"] == 1
 
-        assert result['ChildClass2']['MainClass1'] == 1
-        assert result['ChildClass3']['MainClass1'] == 1
-        assert result['ChildClass5']['MainClass1'] == 0
+        assert result["ChildClass1"]["MainClass1"] == 1
+        assert result["ChildClass1"]["MainClass2"] == 1
+        assert result["ChildClass1"]["MainClass3"] == 1
+        assert result["ChildClass1"]["MainClass4"] == 0
 
-        assert result['ChildClass5']['MainClass4'] == 1
+        assert result["ChildClass2"]["MainClass1"] == 1
+        assert result["ChildClass3"]["MainClass1"] == 1
+        assert result["ChildClass5"]["MainClass1"] == 0
 
-        assert result['ChildClass6']['MainClass5'] == 1
-        assert result['ChildClass7']['MainClass6'] == 1
-        assert result['ChildClass8']['MainClass7'] == 1
+        assert result["ChildClass5"]["MainClass4"] == 1
 
-        assert result['MainClass1'] ['ChildClass1'] == 0
-        assert result['ChildClass2']['ChildClass1'] == 0
+        assert result["ChildClass6"]["MainClass5"] == 1
+        assert result["ChildClass7"]["MainClass6"] == 1
+        assert result["ChildClass8"]["MainClass7"] == 1
+
+        assert result["MainClass1"]["ChildClass1"] == 0
+        assert result["ChildClass2"]["ChildClass1"] == 0
