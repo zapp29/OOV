f848bc84e1380db81866be1c81cfdaf1a7b2c246
Piotr Tchir <ptchir@interia.pl>
2021-07-28 20:05:56 +0200

    black

--- tests/input/dummy_module.py
+++ tests/input/dummy_module.py
@@ -1,55 +1,80 @@
-'''Main classes'''
+"""Main classes"""
+
+
 class MainClass1:
     pass
+
 
 class MainClass2:
     pass
 
+
 class MainClass3:
     pass
+
 
 class MainClass4:
     pass
 
+
 class MainClass5:
     pass
 
+
 class MainClass6:
     pass
+
 
 class MainClass7:
     pass
 
 
-'''Child classes'''
+"""Child classes"""
+
+
 class ChildClass1(MainClass1, MainClass2, MainClass3):
     """Represent many-to-one."""
+
     pass
+
 
 class ChildClass2(MainClass1):
     """Represent one-to-many"""
+
     pass
+
 
 class ChildClass3(MainClass1):
     """Represent one-to-many"""
+
     pass
+
 
 class ChildClass4(MainClass1):
     """Represent one-to-many"""
+
     pass
+
 
 class ChildClass5(MainClass4):
     """Represent one-to-one."""
+
     pass
+
 
 class ChildClass6(MainClass5, MainClass6, MainClass7):
     """Represent many-to-many."""
+
     pass
+
 
 class ChildClass7(MainClass5, MainClass6, MainClass7):
     """Represent many-to-many."""
+
     pass
+
 
 class ChildClass8(MainClass5, MainClass6, MainClass7):
     """Represent many-to-many."""
+
     pass
