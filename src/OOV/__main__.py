4e4ded629032a04f46f23806b5fd265af52c1dfa
Piotr Tchir <ptchir@interia.pl>
2021-08-04 07:15:45 +0200

    add command-line interface

---
+++ src/OOV/__main__.py
@@ -0,0 +1,12 @@
+"""Command-line interface."""
+import click
+
+
+@click.command()
+@click.argument("module", nargs=-1)
+def main(module):
+    click.echo(f"Hello {module}!")
+
+
+if __name__ == "__main__":
+    main()  # pragma: no cover
