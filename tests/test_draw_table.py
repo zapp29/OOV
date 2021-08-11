757f77a6a508d897759c44690d19f61dc0935409
Piotr Tchir <ptchir@interia.pl>
2021-08-11 07:25:20 +0200

    add draw_table simple tests

--- tests/test_draw_table.py
+++ tests/test_draw_table.py
@@ -1,22 +1,18 @@
 """Pytest module testing draw_table.py implementation."""
-import pytest
 from src.oov.draw_table import build_table
 from src.oov.oov import OOV
 
 
-@pytest.fixture
-def load_objects():
-    """Fixture to initialize test scenarios."""
-    return [
-        OOV("itertools"),
-        OOV("types"),
-        OOV("dis"),
-        OOV("typing", "types"),
-        OOV("typing", "itertools"),
-    ]
-
-
-def test_draw_table():
-    """Test draw_table function."""
-    res = OOV("types").view_issubclass()
-    build_table(res)
+def test_draw_table(capsys):
+    """Test if draw_table function returns a string or error."""
+    res = OOV("typing").view_issubclass()
+    try:
+        build_table(res)
+    except Exception:
+        raise AssertionError()
+    finally:
+        pass
+    captured = capsys.readouterr()
+    assert isinstance(captured.out, str)
+    assert len(captured.out) > 100
+    assert len(captured.out.split("\n")) > 50
