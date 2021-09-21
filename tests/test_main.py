767ed91f7e3aff7cb69405cbb8a72778799b1874
Piotr Tchir <ptchir@interia.pl>
2021-09-21 06:38:58 +0200

    add test_main function

--- tests/test_main.py
+++ tests/test_main.py
@@ -18,9 +18,3 @@ def test_main(runner: CliRunner) -> None:
     """It exits with a status code of zero."""
     result = runner.invoke(main, "dis")
     assert result.exit_code == 0
-
-
-def test_console_entry(runner: CliRunner):
-    """It exits with a status code of zero."""
-    result = runner.invoke(main, "types")
-    assert result.exit_code == 0
