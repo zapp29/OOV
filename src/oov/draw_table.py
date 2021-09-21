a1ff5a7c80de1a6dacd748c42054732e29abe13c
Piotr Tchir <ptchir@interia.pl>
2021-09-21 06:41:11 +0200

    implement error index and table

--- src/oov/draw_table.py
+++ src/oov/draw_table.py
@@ -2,6 +2,9 @@
 import subprocess
 from itertools import islice
 from typing import Dict
+from typing import List
+
+from oov.oov import Observation
 
 
 def terminal_line_lenght() -> int:  # pragma: nocover
@@ -10,14 +13,20 @@ def terminal_line_lenght() -> int:  # pragma: nocover
     return int(tput.communicate()[0].strip())
 
 
-def build_table(raw_table: Dict[str, Dict[str, int]]) -> None:
+def build_table(table: Observation) -> None:
     """Prepare a table for printing.
 
     If the table is bigger than screen width, split it into chunks.
     """
+    lens: Dict[str, int]
+    max_col_size: int
+    max_line_length: int
+    raw_table: Dict[str, Dict[str, int]] = table.get_data()
+    table_metadata: Dict[str, Dict[str, int]] = table.get_metadata(format=list)
+
     lens = {e: len(e) for e in raw_table}
     max_col_size = max([len(e) for e in raw_table])
-    max_line_length = int(0.9 * (terminal_line_lenght() - max_col_size))
+    max_line_length = int(0.75 * (terminal_line_lenght() - max_col_size))
 
     joined_header_row = ""
     joined_header_row_dry_run = ""
@@ -40,10 +49,13 @@ def build_table(raw_table: Dict[str, Dict[str, int]]) -> None:
     chunk_end = i
     table_chunk = dict(islice(raw_table.items(), chunk_start, chunk_end))
     draw_table_chunk(table_chunk, lens, max_col_size)
+    draw_error_index(table_metadata)
     return
 
 
-def draw_table_chunk(raw_table: Dict[str, Dict[str, int]], lens, max_col_size) -> None:
+def draw_table_chunk(
+    raw_table: Dict[str, Dict[str, int]], lens: Dict[str, int], max_col_size: int
+) -> None:
     """Render a given chunk of a table."""
     cols = list(raw_table)
     rows = list(raw_table[list(raw_table)[0]])
@@ -60,12 +72,18 @@ def draw_table_chunk(raw_table: Dict[str, Dict[str, int]], lens, max_col_size) -
             l_col = len(col)
 
             try:
-                cell = raw_table[col][row]
+                cell = str(raw_table[col][row])
             except KeyError:
                 cell = "N/A"
-            text = str(cell).rjust(lens[col] - int(l_col / 2)).ljust(l_col + 2)
+            text = cell.rjust(lens[col] - int(l_col / 2)).ljust(l_col + 2)
             text = text.replace("0", " ")
             text = text.replace("1", "Y")
             print(text, end=" ")
         print("")
     return
+
+
+def draw_error_index(table_metadata: List):
+    """Pass."""
+    for row in table_metadata:
+        print(row[0], ":", row[1], row[2])
